4f7db1e4c0e45b0f32e4f73e32c2345cab26cc90
Tales Brunelli <talestb7@gmail.com>
2015-05-05 19:33:09 -0300

    Finalizando o visual do formulário

--- template.php
+++ template.php
@@ -27,12 +27,12 @@
 		<fieldset>
 			<legend>Prioridade</legend>
 			<label>
-				<input type="radio" name="prioridade" value="baixa" checked/>
-				Baixa
+				<input type="radio" name="prioridade" value="baixa"/>
+					Baixa
 				<input type="radio" name="prioridade" value="media"/>
-				Media
+					Media
 				<input type="radio" name="prioridade" value="alta"/>
-				Alta
+					Alta
 			</label>
 		</fieldset>
 		<label>
@@ -45,19 +45,27 @@
 
 <br></br>
 
-
+<p align="center">
 <table>
 	<tr>
 		<th>Tarefas</th>
+		<th>Descricao</th>
+		<th>Prazo</th>
+		<th>Prioridade</th>
+		<th>Concluida</th>
 	</tr>
 
 	<?php foreach ($lista_tarefas as $tarefa) : ?>
 	<tr>
-		<td><?php echo $tarefa; ?> </td>
+		<td><?php echo $tarefa['nome']; ?> </td>
+		<td><?php echo $tarefa['descricao']; ?> </td>
+		<td><?php echo $tarefa['prazo']; ?> </td>
+		<td><?php echo $tarefa['prioridade']; ?> </td>
+		<td><?php echo $tarefa['concluida']; ?> </td>
 	</tr>
 	<?php endforeach; ?>
 </table>
-
+</p>
 
 </body>
 </html>
