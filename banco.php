53f20260b66e4658184da9eb45312fda57a55038
Tales Brunelli <talestb7@gmail.com>
2015-05-12 17:43:14 -0300

    criando o banco de dados

---
+++ banco.php
@@ -0,0 +1,11 @@
+<?php
+$bdServidor = '127.0.0.1'; 
+$bdUsuario = 'sistematarefas'; 
+$bdSenha = 'sistema'; 
+$bdBanco = 'tarefas';
+$conexao = mysqli_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
+if (mysqli_connect_errno($conexao)){ 
+	echo "Problemas para conectar no banco. Verifique os dados!"; 
+	die(); 
+}
+?>
